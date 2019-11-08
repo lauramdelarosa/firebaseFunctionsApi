3f300dda65a1fd4c2f1caa95c70ab33427d5913a
laura De la rosa <laura.dlr.cardona@gmail.com>
2019-11-07 19:31:26 -0500

    mi primer api- un login

---
+++ functions/src/index.ts
@@ -0,0 +1,165 @@
+/* //https://us-central1-sepro-d1621.cloudfunctions.net/webApi/api/v1/warmup
+
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+
+export const webSEPROApi = functions.https.onRequest(main);
+
+app.get('/warmup', (request, response) => {
+
+    response.send('Warming up friend.');
+
+});
+
+
+app.get('/warmupes', (request, response) => {
+    response.send('Warming up friend.');
+
+   /*  admin.firestore().doc("figths").get()
+        .then(snapshot => {
+            const data = snapshot.data()
+            response.send(data)
+        }).catch(error => {
+            response.status(426).send(error);
+        }) */
+});
+/* 
+const db = admin.firestore(); // Add this
+
+
+
+app.post('/fights', async (request, response) => {
+    try {
+        const { winner, losser, title } = request.body;
+        const data = {
+            winner,
+            losser,
+            title
+        }
+        const fightRef = await db.collection('fights').add(data);
+        const fight = await fightRef.get();
+
+        response.json({
+            id: fightRef.id,
+            data: fight.data()
+        });
+
+    } catch (error) {
+
+        response.status(500).send(error);
+
+    }
+});
+
+app.get('/fights/:id', async (request, response) => {
+    try {
+        const fightId = request.params.id;
+
+        if (!fightId) throw new Error('Fight ID is required');
+
+        const fight = await db.collection('fights').doc(fightId).get();
+
+        if (!fight.exists) {
+            throw new Error('Fight doesnt exist.')
+        }
+
+        response.json({
+            id: fight.id,
+            data: fight.data()
+        });
+
+    } catch (error) {
+
+        response.status(500).send(error);
+
+    }
+});
+
+app.get('/fights', async (request, response) => {
+    try {
+
+        const fightQuerySnapshot = await db.collection('fights').get();
+        const fights = [];
+        fightQuerySnapshot.forEach(
+            (doc) => {
+                fights.push({
+                    id: doc.id,
+                    data: doc.data()
+                });
+            }
+        );
+
+        response.json(fights);
+
+    } catch (error) {
+
+        response.status(500).send(error);
+
+    }
+
+});
+
+app.put('/fights/:id', async (request, response) => {
+    try {
+
+        const fightId = request.params.id;
+        const title = request.body.title;
+
+        if (!fightId) throw new Error('id is blank');
+
+        if (!title) throw new Error('Title is required');
+
+        const data = {
+            title
+        };
+        const fightRef = await db.collection('fights')
+            .doc(fightId)
+            .set(data, { merge: true });
+
+        response.json({
+            id: fightId,
+            data
+        })
+
+
+    } catch (error) {
+
+        response.status(500).send(error);
+
+    }
+
+});
+
+app.delete('/fights/:id', async (request, response) => {
+    try {
+
+        const fightId = request.params.id;
+
+        if (!fightId) throw new Error('id is blank');
+
+        await db.collection('fights')
+            .doc(fightId)
+            .delete();
+
+        response.json({
+            id: fightId,
+        })
+
+
+    } catch (error) {
+
+        response.status(500).send(error);
+
+    }
+
+}); */
